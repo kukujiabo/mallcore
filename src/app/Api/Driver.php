1f84bb3f4a2c28f859e13327e8460d10916641f9
kukujiabo <kukujiabo@163.com>
2018-07-29 08:46:02 +0800

    123

---
+++ src/app/Api/Driver.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Api;
+
+/**
+ * 驾驶员接口
+ *
+ */
+class Driver extends BaseApi {
+
+  public function getRules() {
+  
+    return $this->rules(array(
+    
+      'create' => array(
+      
+        'account' => 'account|string|true||账号（手机号）',
+        'password' => 'password|string|true||密码',
+        'name' => 'name|string|true||真实姓名',
+        'city_code' => 'city_code|string|true||所属城市',
+        'remark' => 'remark|string|false||备注'
+      
+      ),
+
+      'getList' => array(
+
+        'account' => 'account|string|false||账号',
+        'name' => 'name|string|false||姓名',
+        'page' => 'page|int|false||页码',
+        'page_size' => 'page_size|int|false|20|每页条数'
+      
+      )
+    
+    ));
+  
+  }
+
+  /**
+   * 新增驾驶员
+   * @desc 新增驾驶员
+   *
+   * @return int 
+   */
+  public function create() {
+  
+    return $this->dm->create($this->retriveRuleParams(__FUNCTION__));
+  
+  }
+
+  /**
+   * 获取驾驶员列表
+   * @desc 获取驾驶员列表
+   *
+   * @return int
+   */
+  public function getList() {
+  
+    return $this->dm->getList($this->retriveRuleParams(__FUNCTION__));
+  
+  }
+
+}
---
+++ src/app/Domain/DriverDm.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Domain;
+
+use App\Service\Transport\DriverSv;
+
+class DriverDm {
+
+  public function create($data) {
+  
+    return DriverSv::create($data);
+  
+  }
+
+  public function getList($data) {
+  
+    return DriverSv::getList($data);
+  
+  }
+
+}
