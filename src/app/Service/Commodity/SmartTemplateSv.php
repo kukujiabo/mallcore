3997a1bc7216f69fce1e76b6247e77f5bc273e9c
kukujiabo <kukujiabo@163.com>
2018-08-15 03:35:46 +0800

    123

--- src/app/Service/Commodity/SmartTemplateSv.php
+++ src/app/Service/Commodity/SmartTemplateSv.php
@@ -8,8 +8,45 @@ class SmartTemplateSv extends BaseService {
 
   public function create($data) {
   
+    $newData = array(
+    
+      'template_name' => $data['template_name'],
+      'layout_ids' => $data['layout_ids'],
+      'min_measure' => $data['min_measure'],
+      'max_measure' => $data['max_measure'],
+      'created_at' => date('Y-m-d H:i:s')
+    
+    );  
   
-  
+    $id = self::add($newData);
+
+    $goods = json_decode($data['goods']);
+
+    $newGoods = [];
+
+    foreach($goods as $good) {
+    
+      $newGood = array(
+      
+        'template_id' => $id,
+
+        'sku_id' => $good['goods_id'],
+
+        'num' => $good['num'],
+
+        'cons_id' => $good['consType'],
+
+        'created_at' => date('Y-m-d H:i:s')
+      
+      ); 
+
+      array_push($newGoods, $newGood);
+    
+    }
+
+    TemplateGoodsSv::batchAdd($newGoods);
+
+    return $id;
   
   }
 
---
+++ src/app/Service/Commodity/TemplateGoodsSv.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Service\Commodity;
+
+use App\Service\BaseService;
+use Core\Service\CurdSv;
+
+class TemplateGoodsSv extends BaseService {
+
+  use CurdSv;
+
+
+}
