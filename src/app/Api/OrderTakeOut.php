df2f2db204eac369d292dc3e3def8b8b9af7e1cf
kukujiabo <kukujiabo@163.com>
2018-08-25 19:55:11 +0800

    13

--- src/app/Api/OrderTakeOut.php
+++ src/app/Api/OrderTakeOut.php
@@ -398,7 +398,7 @@ class OrderTakeOut extends BaseApi {
       'orderAfterSale' => array(
       
         'sn' => 'sn|string|true||订单号',
-        'sku_id' => 'sku_id|int|true||订单商品编号',
+        'sku_id' => 'sku_id|string|true||订单商品编号',
         'num' => 'num|string|true||退货数量'
       
       ),
